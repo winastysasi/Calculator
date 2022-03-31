0054cafebfb7a124f1259a2188b7df5cf207e339
winastysasi <101095816+winastysasi@users.noreply.github.com>
2022-03-31 20:48:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+var icon = document.getElementById("mode");
+    
+icon.onclick = function() {
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")) {
+        icon.title="Light Mode";
+    } else {
+        icon.title="Dark Mode";
+    };
+};
+
+let prevNumber = "";
+let calculationOperator= "";
+let currentNumber = "0";
+
+const clearAll = () => {
+    prevNumber = "";
+    calculationOperator = "";
+    currentNumber = "0";
+}
+
+const inputNumber = (number) => {
+    if (currentNumber === "0") {
+        currentNumber = number;
+    } else {
+        currentNumber += number;
+    };
+};
+
+const inputDecimal = (dot) => {
+    if (currentNumber.includes(".")) {
+        return;
+    };
+    currentNumber += dot;
+};
+
+const inputOperator = (operator) => {
+    if (calculationOperator === "") {
+        prevNumber = currentNumber;
+    };
+    calculationOperator = operator;
+    currentNumber = "0";
+};
+
+const calculatorScreen = document.querySelector(".calculator-screen");
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number;
+};
+
+const numbers = document.querySelectorAll(".number");
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value);
+        updateScreen(currentNumber);
+    });
+});
+
+const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value);
+    });
+});
+
+const equalSign = document.querySelector(".equal-sign");
+
+equalSign.addEventListener("click", () => {
+    calculate();
+    updateScreen(currentNumber);
+});
+
+const calculate = () => {
+    let result = "";
+    switch (calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber);
+            break;
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber);
+            break;
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber);
+            break;
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber);
+            break;
+        default:
+            return;
+    }
+    currentNumber = result;
+    calculationOperato = "";
+};
+
+const clearBtn = document.querySelector(".all-clear");
+
+clearBtn.addEventListener("click", () => {
+    clearAll();
+    updateScreen(currentNumber);
+});
+
+const decimal = document.querySelector(".decimal");
+
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value);
+    updateScreen(currentNumber);
+});
